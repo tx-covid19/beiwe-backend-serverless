c22f8b5e564217a043ac0049ea6b62ad367d1684
Aaron Klein <ajmath62@gmail.com>
2018-04-19 17:26:44 -0400

    Fixed bug with ID/Object ID in pipeline

--- api/data_pipeline_api.py
+++ api/data_pipeline_api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, flash, redirect
 
+from database.models import Study
 from libs.admin_authentication import authenticate_admin_study_access
 from pipeline.boto_helpers import get_aws_object_names
 from pipeline.index import create_one_job, refresh_data_access_credentials
@@ -13,15 +14,18 @@ data_pipeline_api = Blueprint('data_pipeline_api', __name__)
 def run_manual_code(study_id):
     """
     Create an AWS Batch job for the Study specified
-    :param study_id: ObjectId of a Study
+    :param study_id: Primary key of a Study
     """
+    
+    # Get the object ID of the study, used in the pipeline
+    object_id = Study.objects.get(pk=study_id).object_id
     
     # Get new data access credentials for the manual user
     aws_object_names = get_aws_object_names()
     refresh_data_access_credentials('manually', aws_object_names)
     
     # Submit a manual job
-    create_one_job('manually', study_id)
+    create_one_job('manually', object_id)
     
     # The success message gets displayed to the user upon redirect
     flash('Data pipeline code successfully initiated!', 'success')
