03ab4ffdd62bbaaec89d15016efa65e8e81134c0
Eli Jones <eli@zagaran.com>
2018-06-04 21:05:10 -0400

    modifies creating error sentries to make them a little less obnoxious in test environments.

--- libs/sentry.py
+++ libs/sentry.py
@@ -1,7 +1,8 @@
-from cronutils import ErrorSentry
+from cronutils import ErrorSentry, ErrorHandler
 from raven import Client as SentryClient
 from raven.transport import HTTPTransport
-
+from raven.exceptions import InvalidDsn
+from libs.logging import log_error
 from config.settings import SENTRY_ANDROID_DSN, SENTRY_DATA_PROCESSING_DSN, SENTRY_ELASTIC_BEANSTALK_DSN, SENTRY_JAVASCRIPT_DSN
 
 
@@ -22,9 +23,13 @@ def make_sentry_client(sentry_type, tags=None):
     dsn = get_dsn_from_string(sentry_type)
     tags = tags or {}
     return SentryClient(dsn=dsn, tags=tags, transport=HTTPTransport)
-
+    
 
 def make_error_sentry(sentry_type, tags=None):
     dsn = get_dsn_from_string(sentry_type)
     tags = tags or {}
-    return ErrorSentry(dsn, sentry_client_kwargs={'tags': tags, 'transport': HTTPTransport})
+    try:
+        return ErrorSentry(dsn, sentry_client_kwargs={'tags': tags, 'transport': HTTPTransport})
+    except InvalidDsn as e:
+        log_error(e)
+        return ErrorHandler()
