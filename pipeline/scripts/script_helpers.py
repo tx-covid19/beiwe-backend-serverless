a21f9eb3594c565276133d24dc120f0d933f5446
Eli Jones <eli@zagaran.com>
2018-06-04 21:12:16 -0400

    woops again, adds script_helpers.py

---
+++ pipeline/scripts/script_helpers.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Do not modify this * import, this is how we solve all the pipeline/scripts folder's import problems
+from ..configuration_getters import custom_config_components
+
+
+def set_default_region():
+    aws_object_names = custom_config_components()
+    region_name = aws_object_names['region_name']
+    subprocess.check_call(['aws', 'configure', 'set', 'default.region', region_name])
+
