3bd895becd33bb3822f1683bb0e3834e301bd912
Eli Jones <eli@zagaran.com>
2019-06-13 15:46:42 -0400

    [dashboard] adds dashboard color settings and related models along with basic access thereof.

--- database/study_models.py
+++ database/study_models.py
@@ -246,3 +246,53 @@ class DeviceSettings(AbstractModel):
     consent_sections = JSONTextField(default=DEFAULT_CONSENT_SECTIONS_JSON)
 
     study = models.OneToOneField('Study', on_delete=models.PROTECT, related_name='device_settings')
+
+
+class DashboardColorSetting(AbstractModel):
+    """ Database model, details of color settings point at this model. """
+    data_type = models.CharField(max_length=32)
+    study = models.ForeignKey("Study", on_delete=models.PROTECT, related_name="dashboard_colors")
+
+    class Meta:
+        # only one of these color settings per-study-per-data type
+        unique_together = (("data_type", "study"),)
+
+    def get_dashboard_color_settings(self):
+        # return a (json serializable) dict of a dict of the gradient and a list of dicts for
+        # the inflection points.
+
+        # Safely/gracefully access the gradient's one-to-one field.
+        try:
+            gradient = {
+                "color_range_min": self.gradient.color_range_min,
+                "color_range_max": self.gradient.color_range_max,
+            }
+        except DashboardGradient.DoesNotExist:
+            gradient = {}
+
+        return {
+            "gradient": gradient,
+            "inflections": list(self.inflections.values("operator", "inflection_point")),
+        }
+
+class DashboardGradient(AbstractModel):
+    # It should be the case that there is only one gradient per DashboardColorSettings
+    dashboard_color_setting = models.OneToOneField(
+        DashboardColorSetting, on_delete=models.PROTECT, related_name="gradient", unique=True,
+    )
+
+    # By setting both of these to 0 the frontend will automatically use tha biggest and smallest
+    # values on the current page.
+    color_range_min = models.IntegerField(default=0)
+    color_range_max = models.IntegerField(default=0)
+
+
+class DashboardInflection(AbstractModel):
+    dashboard_color_setting = models.ForeignKey(
+        DashboardColorSetting, on_delete=models.PROTECT, related_name="inflections"
+    )
+
+    # these are a mathematical operator and a numerical "inflection point"
+    # no default for the operator, default of 0 is safe.
+    operator = models.CharField(max_length=1)
+    inflection_point = models.IntegerField(default=0)
