2ba299c7b42baf0720df0d7af23d519beda0b7d4
Eli Jones <eli@zagaran.com>
2019-07-19 17:45:19 -0400

    [development] updates populate chunk script to allow partial updates.

--- scripts/populate_chunk_file_size.py
+++ scripts/populate_chunk_file_size.py
@@ -12,10 +12,22 @@ from database.data_access_models import ChunkRegistry
 from libs.s3 import conn, S3_BUCKET
 
 print("start:", datetime.now())
-query = ChunkRegistry.objects.filter(file_size__isnull=True).values_list("pk", "chunk_path")
+
+# stick study object ids here to process particular studies
+study_object_ids = []
+
+filters = dict(file_size__isnull=True)
+if study_object_ids:
+    filters["study__object_id__in"] = study_object_ids
+
+# this could be a huge query, use the iterator
+query = ChunkRegistry.objects.filter(**filters).values_list("pk", "chunk_path").iterator()
+
+
 for i, (pk, path) in enumerate(query):
     if i % 1000 == 0:
         print(i)
-    size = conn.get_object(Bucket=S3_BUCKET, Key=path)["ContentLength"]
+    size = conn.head_object(Bucket=S3_BUCKET, Key=path)["ContentLength"]
     ChunkRegistry.objects.filter(pk=pk).update(file_size=size)
+
 print("end:", datetime.now())
