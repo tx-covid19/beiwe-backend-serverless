f26ebf527b33a051ec652cb1cf762e28f48bd1c3
Eli Jones <eli@zagaran.com>
2019-10-17 14:52:37 -0400

    [downtime-fix] [downtime] keep this commit rebased at the top of the downtime branch, this branch forces beiwe into downtime mode.

--- wsgi.py
+++ wsgi.py
@@ -3,16 +3,4 @@ from os.path import abspath
 current_folder = abspath(__file__).rsplit('/', 1)[0]
 sys.path.insert(0, current_folder)
 sys.stdout = sys.stderr
-
-# On this branch there is no content
-
-if sys.version_info > (3, 0):
-    # Only run codebase if python 3 is detected
-    from app import app as application
-else:
-    # run downtime mode if not running in python 3.
-    # there is a branch, "downtime", for which the above check is skipped and requirements.txt is
-    # minimal.  In order for python 2-3 upgrade to work successfully the deployment must be set to
-    # run in downtime mode.
-    from downtime import app as application
-
+from downtime import app as application
