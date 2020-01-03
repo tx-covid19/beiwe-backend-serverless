667c7336f78af7f98e99001015bf03b311f7b8e2
cameron craddock <cameron.craddock@gmail.com>
2020-01-03 19:00:48 +0000

    consolidated functionality for interfacing s3; moved in functions for constructing raw and chunk file paths; moved out client key management

--- libs/s3.py
+++ libs/s3.py
@@ -1,22 +1,69 @@
 import boto3
+import os
 
-from config.constants import DEFAULT_S3_RETRIES
+from config.constants import DEFAULT_S3_RETRIES, KEY_FOLDER, RAW_DATA_FOLDER, CHUNKS_FOLDER, API_TIME_FORMAT, CHUNK_TIMESLICE_QUANTUM
 from config.settings import (S3_BUCKET, BEIWE_SERVER_AWS_ACCESS_KEY_ID,
     BEIWE_SERVER_AWS_SECRET_ACCESS_KEY, S3_REGION_NAME)
 from libs import encryption
+from botocore.exceptions import ClientError
+from datetime import datetime
 
 conn = boto3.client('s3',
                     aws_access_key_id=BEIWE_SERVER_AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=BEIWE_SERVER_AWS_SECRET_ACCESS_KEY,
                     region_name=S3_REGION_NAME)
 
+def unix_time_to_string(unix_time):
+    return datetime.utcfromtimestamp(unix_time).strftime( API_TIME_FORMAT )
+
+def s3_delete(key_path, study_object_id, raw_path=False):
+    if not raw_path:
+        key_path = os.path.join(study_object_id, key_path)
+    conn.delete_object(Bucket=S3_BUCKET, Key=key_path)
+
+def s3_exists(key_path, study_object_id, raw_path=False):
+
+    return_value = True
+
+    if not raw_path:
+        key_path = os.path.join(study_object_id, key_path)
+
+    try:
+        conn.head_object(Bucket=S3_BUCKET, Key=key_path)
+    except ClientError:
+        return_value = False
+
+    return return_value
+        
+def s3_move(source_key_path, destination_key_path, study_object_id, raw_path=False):
+
+    if not raw_path:
+        source_key_path = os.path.join(study_object_id, source_key_path)
+        destination_key_path = os.path.join(study_object_id, destination_key_path)
+
+    try:
+        conn.copy_object(CopySource={'Bucket': S3_BUCKET, 'Key': source_key_path}, 
+                         Bucket=S3_BUCKET, Key=destination_key_path)
+    except:
+        print('Could not copy key {0} {1}'.format(S3_BUCKET, source_key_path))
+        raise
+
+    conn.delete_object(Bucket=S3_BUCKET, Key=source_key_path)
 
 def s3_upload(key_path, data_string, study_object_id, raw_path=False):
+
     if not raw_path:
         key_path = study_object_id + "/" + key_path
     data = encryption.encrypt_for_server(data_string, study_object_id)
     conn.put_object(Body=data, Bucket=S3_BUCKET, Key=key_path, ContentType='string')
 
+def s3_upload_public(key_path, data_string, study_object_id, raw_path=False):
+
+    if not raw_path:
+        key_path = study_object_id + "/" + key_path
+
+    conn.put_object(Body=data_string, Bucket=S3_BUCKET, Key=key_path, ContentType='string', ACL='public-read')
+    return 'https://{0}.s3.amazonaws.com/{1}'.format(S3_BUCKET, key_path)
 
 def s3_retrieve(key_path, study_object_id, raw_path=False, number_retries=DEFAULT_S3_RETRIES):
     """ Takes an S3 file path (key_path), and a study ID.  Takes an optional argument, raw_path,
@@ -68,33 +115,25 @@ def _do_list_files_generator(page_iterator):
             yield item['Key'].strip("/")
 
 
-def s3_delete(key_path):
-    raise Exception("NO DONT DELETE")
+"""################################ S3 PATHS ################################"""
 
-################################################################################
-######################### Client Key Management ################################
-################################################################################
+def construct_s3_key_paths(study_id, participant_id):
+    """ S3 file paths for chunks are of this form:
+        RAW_DATA/study_id/user_id/data_type/time_bin.csv """
+    return {'private': os.path.join(KEY_FOLDER, study_id, participant_id + "_private"),
+            'public': os.path.join(KEY_FOLDER, study_id, participant_id + "_public")}
 
-def create_client_key_pair(patient_id, study_id):
-    """Generate key pairing, push to database, return sanitized key for client."""
-    public, private = encryption.generate_key_pairing()
-    s3_upload("keys/" + patient_id + "_private", private, study_id )
-    s3_upload("keys/" + patient_id + "_public", public, study_id )
+def construct_s3_raw_data_path(study_id, filename):
+    """ S3 file paths for chunks are of this form:
+        RAW_DATA/study_id/user_id/data_type/time_bin.csv """
+    return os.path.join(RAW_DATA_FOLDER, study_id, filename)
 
+def construct_s3_chunk_path(study_id, user_id, data_type, time_bin):
+    """ S3 file paths for chunks are of this form:
+        CHUNKED_DATA/study_id/user_id/data_type/time_bin.csv """
+    return "%s/%s/%s/%s/%s.csv" % (CHUNKS_FOLDER, study_id, user_id, data_type,
+        unix_time_to_string(time_bin*CHUNK_TIMESLICE_QUANTUM) )
 
-def get_client_public_key_string(patient_id, study_id):
-    """Grabs a user's public key string from s3."""
-    key_string = s3_retrieve( "keys/" + patient_id +"_public" , study_id)
-    return encryption.prepare_X509_key_for_java( key_string )
+def construct_s3_chunk_path_from_raw_data_path(raw_data_path):
+    return raw_data_path.replace(RAW_DATA_FOLDER, CHUNKS_FOLDER)
 
-
-def get_client_public_key(patient_id, study_id):
-    """Grabs a user's public key file from s3."""
-    key = s3_retrieve( "keys/" + patient_id +"_public", study_id )
-    return encryption.import_RSA_key( key )
-
-
-def get_client_private_key(patient_id, study_id):
-    """Grabs a user's private key file from s3."""
-    key = s3_retrieve( "keys/" + patient_id +"_private", study_id)
-    return encryption.import_RSA_key( key )
