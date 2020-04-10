ca3f29651a4fea931efff56ea9bf97f16466b018
Eli Jones <eli@zagaran.com>
2020-04-09 20:14:34 -0400

    [data-download-api] data download script is now python 3 compatible and uses the requests library.

--- data_acces_api_reference/download_data.py
+++ data_acces_api_reference/download_data.py
@@ -1,33 +1,58 @@
 import io
 import json
 import os
-import urllib.parse
-import urllib.request
 import zipfile
+
 from os import path
+
+try:
+    import requests
+except ImportError as e:
+    print(str(e))
+    print("\n\nThis scripts uses the python requests library, it is not installed.")
+    exit(1)
+
 
 # Comment out the following import to disable the credentials file.
 try:
     from my_data_access_api_credentials import ACCESS_KEY, SECRET_KEY, API_URL_BASE
 except ImportError:
-    ACCESS_KEY, SECRET_KEY, API_URL_BASE = None, None, None
+    ACCESS_KEY, SECRET_KEY, API_URL_BASE = None, None, ""
 
+# used in commented out code
 API_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+
+# Data Streams
 ACCELEROMETER = "accelerometer"
 BLUETOOTH = "bluetooth"
 CALL_LOG = "calls"
 GPS = "gps"
 IDENTIFIERS = "identifiers"
-ANDROID_LOG = "app_log"
+ANDROID_LOG_FILE = "app_log"
+IOS_LOG_FILE = "ios_log"
 POWER_STATE = "power_state"
 SURVEY_ANSWERS = "survey_answers"
 SURVEY_TIMINGS = "survey_timings"
 TEXTS_LOG = "texts"
 VOICE_RECORDING = "audio_recordings"
+IMAGE_FILE = "image_survey"
 WIFI = "wifi"
-IOS_LOG = "ios_log"
+PROXIMITY = "proximity"
+GYRO = "gyro"
+MAGNETOMETER = "magnetometer"
+DEVICEMOTION = "devicemotion"
+REACHABILITY = "reachability"
 
-DEBUG = False
+
+# correct bad urls for sanity...
+if not API_URL_BASE:
+    print("You need to define the variable API_URL_BASE, this is the domain of your Beiwe deployment.")
+    exit(1)
+if not API_URL_BASE.startswith("https://"):
+    API_URL_BASE = "https://" + API_URL_BASE
+if not API_URL_BASE.endswith("/"):
+    API_URL_BASE = API_URL_BASE + "/"
 
 
 def make_request(study_id, access_key=ACCESS_KEY, secret_key=SECRET_KEY, user_ids=None, data_streams=None,
@@ -66,12 +91,16 @@ def make_request(study_id, access_key=ACCESS_KEY, secret_key=SECRET_KEY, user_id
         raise Exception("You must provide credentials to run this API call.")
 
     url = API_URL_BASE + 'get-data/v1'
-    values = {'access_key':access_key,
-              'secret_key':secret_key,
-              'study_id':study_id}
+    values = {
+        'access_key': access_key,
+        'secret_key': secret_key,
+        'study_id': study_id,
+    }
 
-    if user_ids: values['user_ids'] = json.dumps(user_ids)
-    if data_streams: values['data_streams'] = json.dumps(data_streams)
+    if user_ids:
+        values['user_ids'] = json.dumps(user_ids)
+    if data_streams:
+        values['data_streams'] = json.dumps(data_streams)
 
     # Uncomment the below lines to enable (time zone unaware) datetime object support, add 'from datetime import datetime' to the imports.
     if time_start:
@@ -91,41 +120,12 @@ def make_request(study_id, access_key=ACCESS_KEY, secret_key=SECRET_KEY, user_id
     else:
         old_registry = {}
 
-    print("sending request, this could take some time.")
-
-    req = urllib.request.Request(url, data=bytes(json.dumps(values), encoding="utf-8"), method='POST')
-    response = urllib.request.urlopen(req)
-
-    if DEBUG == False:
-        return_data = response.read()
-    else:
-        import gc
-        from datetime import datetime
-        z = datetime.now()
-        data_list = []
-        profile_list = []
-        chunk_size = 2**16
-        while True:
-            a = datetime.now()
-            data = response.read(chunk_size)
-            if not data:
-                print("done, average of %s MB/s" % (sum(profile_list) / len(profile_list)))
-                print("download took %s seconds." % (datetime.now() - z).total_seconds())
-                break
-            data_list.append(data)
-            b = datetime.now()
-            total = (len(data_list) * chunk_size / 1024. / 1024.)
-            speed = (chunk_size / 1024. / 1024.) / (b - a).total_seconds()
-            profile_list.append(speed)
-            print("%s MB downloaded @ %s MB/s" % (total, speed))
-
-        return_data = "".join(data_list)
-        del data_list
-        gc.collect()
-
+    print("sending request, receiving data, this could take some time.")
+    response = requests.post(url, data=values)
+    data = response.content
     print("Data received.  Unpacking and overwriting any updated files into", path.abspath('.'))
 
-    z = zipfile.ZipFile(io.StringIO(return_data))
+    z = zipfile.ZipFile(io.BytesIO(data))
     z.extractall()
 
     with open("registry") as f:
@@ -144,21 +144,21 @@ def make_request(study_id, access_key=ACCESS_KEY, secret_key=SECRET_KEY, user_id
 def get_users_request(study_id, access_key=ACCESS_KEY, secret_key=SECRET_KEY):
     """ Provides a list of user ids enrolled in the given study. """
     url = API_URL_BASE + 'get-users/v1'
-    values = {'access_key':access_key,
-              'secret_key':secret_key,
-              'study_id':study_id}
-
-    req = urllib.request.Request(url, urllib.parse.urlencode(values))
-    response = urllib.request.urlopen(req)
-    return json.loads(response.read())
+    values = {
+        'access_key': access_key,
+        'secret_key': secret_key,
+        'study_id': study_id,
+    }
+    data = requests.post(url, data=values).content.decode()
+    return json.loads(data)
 
 
 def get_studies_request(access_key=ACCESS_KEY, secret_key=SECRET_KEY):
     """ Provides a dictionary of the form {study_key:study_name} for studies accessible to the provided user credentials"""
     url = API_URL_BASE + 'get-studies/v1'
-    values = {'access_key':access_key,
-              'secret_key':secret_key}
-
-    req = urllib.request.Request(url, urllib.parse.urlencode(values))
-    response = urllib.request.urlopen(req)
-    return json.loads(response.read())
+    values = {
+        'access_key': access_key,
+        'secret_key': secret_key,
+    }
+    data = requests.post(url, data=values).content.decode()
+    return json.loads(data)
