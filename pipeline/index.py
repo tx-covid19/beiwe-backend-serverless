b36b08922547bdd6335da959e07ebd2053f545c9
ccraddock <cameron.craddock@gmail.com>
2020-04-21 14:58:57 +0000

    added support for copy to box

--- pipeline/index.py
+++ pipeline/index.py
@@ -10,6 +10,8 @@ from pipeline.configuration_getters import get_eb_config, get_generic_config
 
 from config.constants import API_TIME_FORMAT
 import datetime
+import os
+
 
 def str_to_datetime(time_string):
     """ Translates a time string to a datetime object, raises a 400 if the format is wrong."""
@@ -19,15 +21,20 @@ def str_to_datetime(time_string):
         if "does not match format" in e.message:
             return abort(400)
 
+
 def refresh_data_access_credentials(freq, ssm_client=None, webserver=False):
     """
     Refresh the data access credentials for a particular BATCH USER user and upload them
     (encrypted) to the AWS Parameter Store. This enables AWS batch jobs to get the
     credentials and thereby access the data access API (DAA).
-    :param freq: string, one of 'hourly' | 'daily' | 'weekly' | 'monthly' | 'manually'
     This is used to know what call the data access credentials on AWS.
+
+    :param freq: string, one of 'hourly' | 'daily' | 'weekly' | 'monthly' | 'manually'
+    :param ssm_client: optional, ssm_client to use if one is already available
+    :param webserver: whether or not the method is called from the webserver as opposed to API
+    :return:
     """
-    
+
     # Get or create Researcher with no password. This means that nobody can log in as this
     # Researcher in the web interface.
     researcher_name = 'BATCH USER {}'.format(freq)
@@ -40,7 +47,7 @@ def refresh_data_access_credentials(freq, ssm_client=None, webserver=False):
 
     # Ensure that the Researcher is attached to all Studies. This allows them to access all
     # data via the DAA.
-            #is_batch_user=True,
+    # is_batch_user=True,
     for study in Study.objects.all():
         StudyRelation.objects.get_or_create(
             study=study,
@@ -78,20 +85,33 @@ def refresh_data_access_credentials(freq, ssm_client=None, webserver=False):
     )
 
 
-def create_one_job(freq, object_id, owner_id, destination_email_addresses='', data_start_datetime='', data_end_datetime='', participants='', job_type='run_pipeline', box_directory='', client=None, webserver=False):
+def create_one_job(freq, object_id, owner_id, pipeline_function, destination_email_addresses='', data_start_datetime='',
+                   data_end_datetime='', participants='', job_type='run_pipeline', box_directory='',
+                   datastreams='', client=None, webserver=False):
     """
     Create an AWS batch job
     The aws_object_names and client parameters are optional. They are provided in case
     that this function is run as part of a loop, to avoid an unnecessarily large number of
     file operations or API calls.
-    :param freq: string e.g. 'daily', 'manually'
-    :param a Study database object
-    :param a string of a patient id
-    :param client: a credentialed boto3 client or None
-    
+
     config needs are the following: job_name, job_defn_name, queue_name
+
+    :param freq: string e.g. 'daily', 'manually'
+    :param object_id: a Study database object
+    :param owner_id: a string, the username for the researcher executing the pipeline job
+    :param pipeline_function: string containing the name of the pipeline function to execute
+    :param destination_email_addresses: email address where status updates should be sent, optional
+    :param data_start_datetime: start time for data query, optional
+    :param data_end_datetime:  end time for data query, optional
+    :param participants: comma separated string of patient_ids for participants whose data should be processed
+    :param job_type: copy_to_box or run_pipeline, what are we doing?
+    :param box_directory: directory on box to copy data into
+    :param datastreams: the datastreams to perform the submitted operation on
+    :param client: a credentialed boto3 client or None
+    :param webserver: whether or not this was called from the webserver as opposed to a API
+    :return:
     """
-    
+
     # Get the AWS parameters and client if not provided
     if not webserver:
         aws_object_names = get_generic_config()
@@ -100,13 +120,19 @@ def create_one_job(freq, object_id, owner_id, destination_email_addresses='', da
 
     # requires region_name be defined.
     if client is None:
-        client = get_boto_client('batch')
+        client = get_boto_client('batch', os.getenv("pipeline_region", None))
 
     # clean up list of participants
     if isinstance(participants, list):
         participants = " ".join(participants)
     elif ',' in participants:
         participants = " ".join(participants.split(','))
+
+    # clean up the list of datastreams
+    if isinstance(datastreams, list):
+        datastreams = " ".join(datastreams)
+    elif ',' in datastreams:
+        datastreams = " ".join(datastreams.split(','))
 
     # clean up list of destination email addresses
     if isinstance(destination_email_addresses, list):
@@ -117,11 +143,12 @@ def create_one_job(freq, object_id, owner_id, destination_email_addresses='', da
     if job_type not in ALL_JOB_TYPES:
         raise ValueError(f"unknown job type {job_type}")
 
+    request_datetime = datetime.datetime.now()
 
     print(f"scheduling {job_type} job for study {Study.objects.get(object_id=object_id).id}")
     pipeline_id = PipelineExecutionTracking.pipeline_scheduled(owner_id,
                                                                Study.objects.get(object_id=object_id).id,
-                                                               datetime.datetime.now(),
+                                                               request_datetime,
                                                                destination_email_addresses,
                                                                data_start_datetime,
                                                                data_end_datetime,
@@ -129,7 +156,6 @@ def create_one_job(freq, object_id, owner_id, destination_email_addresses='', da
                                                                job_type=job_type,
                                                                box_directory=box_directory)
 
-    response = None
     try:
         response = client.submit_job(
             jobName=aws_object_names['job_name'].format(freq=freq),
@@ -137,6 +163,18 @@ def create_one_job(freq, object_id, owner_id, destination_email_addresses='', da
             jobQueue=aws_object_names['queue_name'],
             containerOverrides={
                 'environment': [
+                    {
+                        'name': 'owner_id',
+                        'value': str(owner_id),
+                    },
+                    {
+                        'name': 'request_datetime',
+                        'value': request_datetime.strftime(API_TIME_FORMAT),
+                    },
+                    {
+                        'name': 'pipeline_function',
+                        'value': pipeline_function,
+                    },
                     {
                         'name': 'pipeline_id',
                         'value': str(pipeline_id),
@@ -146,16 +184,8 @@ def create_one_job(freq, object_id, owner_id, destination_email_addresses='', da
                         'value': str(object_id),
                     },
                     {
-                        'name': 'study_name',
-                        'value': Study.objects.get(object_id=object_id).name,
-                    },
-                    {
                         'name': 'FREQ',
                         'value': freq,
-                    },
-                    {
-                        'name': 'job_type',
-                        'value': job_type,
                     },
                     {
                         'name': 'destination_email_address',
@@ -163,21 +193,25 @@ def create_one_job(freq, object_id, owner_id, destination_email_addresses='', da
                     },
                     {
                         'name': 'data_start_datetime',
-                        'value': data_start_datetime,
+                        'value': data_start_datetime.strftime(API_TIME_FORMAT) if data_start_datetime else '',
                     },
                     {
                         'name': 'data_end_datetime',
-                        'value': data_end_datetime,
+                        'value': data_end_datetime.strftime(API_TIME_FORMAT) if data_end_datetime else '',
                     },
                     {
                         'name': 'participants',
                         'value': participants,
                     },
                     {
+                        'name': 'datastreams',
+                        'value': datastreams,
+                    },
+                    {
                         'name': 'box_directory',
                         'value': box_directory,
                     }
-                ],
+                ]
             },
         )
 
@@ -237,16 +271,14 @@ def weekly():
 def monthly():
     create_all_jobs('monthly')
 
-def terminate_job(pipeline_id, user_id, client=None):
 
-    # Get the AWS parameters and client if not provided
-    aws_object_names = get_generic_config()
+def terminate_job(pipeline_id, user_id, client=None):
 
     # requires region_name be defined.
     if client is None:
         client = get_boto_client('batch')
 
-    pipeline = PipelineExecutionTracking.objects.get(id = pipeline_id)
+    pipeline = PipelineExecutionTracking.objects.get(id=pipeline_id)
 
     if not pipeline.batch_job_id:
         raise ValueError(f'Error terminating pipeline {pipeline_id}, batch job id not found')
