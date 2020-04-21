99aee2798ae2724d9ea5972f722afc5d9a40d829
ccraddock <cameron.craddock@gmail.com>
2020-04-21 14:49:22 +0000

    pep8 formatting, added constant for number of threads to use for pipeline jobs

--- config/constants.py
+++ config/constants.py
@@ -2,26 +2,26 @@ from os import getenv
 
 from config.settings import DOMAIN_NAME
 
-### Environment settings ###
+# Environment settings
 # All settings here can be configured by setting an environment variable, or by editing the default value
 
 # To customize any of these values, use the following pattern.
 # DEFAULT_S3_RETRIES = getenv("DEFAULT_S3_RETRIES") or 10
 # Note that this file is _not_ in the gitignore.
 
-## Networking
-#This value is used in libs.s3, does what it says.
+# Networking
+# This value is used in libs.s3, does what it says.
 DEFAULT_S3_RETRIES = getenv("DEFAULT_S3_RETRIES") or 3
 
-## File processing directives
-#NOTE: these numbers were determined through trial and error on a C4 Large AWS instance.
-#Used in data download and data processing, base this on CPU core count.
+# File processing directives
+# NOTE: these numbers were determined through trial and error on a C4 Large AWS instance.
+# Used in data download and data processing, base this on CPU core count.
 CONCURRENT_NETWORK_OPS = getenv("CONCURRENT_NETWORK_OPS") or 10
-#Used in file processing, number of files to be pulled in and processed simultaneously.
+# Used in file processing, number of files to be pulled in and processed simultaneously.
 # Higher values reduce s3 usage, reduce processing time, but increase ram requirements.
 FILE_PROCESS_PAGE_SIZE = getenv("FILE_PROCESS_PAGE_SIZE") or 250
 
-#This string will be printed into non-error hourly reports to improve error filtering.
+# This string will be printed into non-error hourly reports to improve error filtering.
 DATA_PROCESSING_NO_ERROR_STRING = getenv("DATA_PROCESSING_NO_ERROR_STRING") or "2HEnBwlawY"
 
 # The number of minutes after which a queued celery task will be invalidated.
@@ -29,8 +29,7 @@ DATA_PROCESSING_NO_ERROR_STRING = getenv("DATA_PROCESSING_NO_ERROR_STRING") or "
 CELERY_EXPIRY_MINUTES = getenv("CELERY_EXPIRY_MINUTES") or 14
 CELERY_ERROR_REPORT_TIMEOUT_SECONDS = getenv("CELERY_ERROR_REPORT_TIMEOUT_SECONDS") or 60*15
 
-
-## Data streams and survey types ##
+# Data streams and survey types
 ALLOWED_EXTENSIONS = {'csv', 'json', 'mp4', "wav", 'txt', 'jpg'}
 PROCESSABLE_FILE_EXTENSIONS = [".csv", ".mp4", ".wav"]
 # These don't appear to be used...
@@ -39,7 +38,7 @@ FILE_TYPES = ['gps', 'accel', 'voiceRecording', 'powerState', 'callLog', 'textLo
               'bluetoothLog', 'surveyAnswers', 'surveyTimings', 'imageSurvey']
 
 
-## HTML lists ##
+# HTML lists
 CHECKBOX_TOGGLES = ["accelerometer",
                     "gps",
                     "calls",
@@ -76,7 +75,7 @@ TIMER_VALUES = ["accelerometer_off_duration_seconds",
                 "magnetometer_off_duration_seconds",
                 "magnetometer_on_duration_seconds",
                 "devicemotion_off_duration_seconds",
-                "devicemotion_on_duration_seconds" ]
+                "devicemotion_on_duration_seconds"]
 
 # The format that dates should be in throughout the codebase
 API_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
@@ -86,7 +85,7 @@ API_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
 # used in the dashboard
 REDUCED_API_TIME_FORMAT = "%Y-%m-%d"
 
-## Chunks
+# Chunks
 # This value is in seconds, it sets the time period that chunked files will be sliced into.
 CHUNK_TIMESLICE_QUANTUM = 3600
 # the name of the s3 folder that contains chunked data
@@ -95,7 +94,7 @@ RAW_DATA_FOLDER = "RAW_DATA"
 CHUNKS_FOLDER = "CHUNKED_DATA"
 PIPELINE_FOLDER = "PIPELINE_DATA"
 
-## Constants for for the keys in data_stream_to_s3_file_name_string
+# Constants for for the keys in data_stream_to_s3_file_name_string
 ACCELEROMETER = "accelerometer"
 BLUETOOTH = "bluetooth"
 CALL_LOG = "calls"
@@ -242,26 +241,46 @@ REVERSE_UPLOAD_FILE_TYPE_MAPPING = {v: k for k, v in UPLOAD_FILE_TYPE_MAPPING.it
 def data_stream_to_s3_file_name_string(data_type):
     """Maps a data type to the internal string representation used throughout the codebase.
         (could be a dict mapping, but it is fine) """
-    if data_type == ACCELEROMETER: return "accel"
-    if data_type == BLUETOOTH: return "bluetoothLog"
-    if data_type == CALL_LOG: return "callLog"
-    if data_type == GPS: return "gps"
-    if data_type == IDENTIFIERS: return "identifiers"
-    if data_type == ANDROID_LOG_FILE: return "logFile"
-    if data_type == POWER_STATE: return "powerState"
-    if data_type == SURVEY_ANSWERS: return "surveyAnswers"
-    if data_type == SURVEY_TIMINGS: return "surveyTimings"
-    if data_type == TEXTS_LOG: return "textsLog"
-    if data_type == VOICE_RECORDING: return "voiceRecording"
-    if data_type == WIFI: return "wifiLog"
-    if data_type == PROXIMITY: return "proximity"
-    if data_type == GYRO: return "gyro"
-    if data_type == MAGNETOMETER: return "magnetometer"
-    if data_type == DEVICEMOTION: return "devicemotion"
-    if data_type == REACHABILITY: return "reachability"
-    if data_type == IOS_LOG_FILE: return "ios_log"
-    if data_type == IMAGE_FILE: "imageSurvey"
+    if data_type == ACCELEROMETER:
+        return "accel"
+    if data_type == BLUETOOTH:
+        return "bluetoothLog"
+    if data_type == CALL_LOG:
+        return "callLog"
+    if data_type == GPS:
+        return "gps"
+    if data_type == IDENTIFIERS:
+        return "identifiers"
+    if data_type == ANDROID_LOG_FILE:
+        return "logFile"
+    if data_type == POWER_STATE:
+        return "powerState"
+    if data_type == SURVEY_ANSWERS:
+        return "surveyAnswers"
+    if data_type == SURVEY_TIMINGS:
+        return "surveyTimings"
+    if data_type == TEXTS_LOG:
+        return "textsLog"
+    if data_type == VOICE_RECORDING:
+        return "voiceRecording"
+    if data_type == WIFI:
+        return "wifiLog"
+    if data_type == PROXIMITY:
+        return "proximity"
+    if data_type == GYRO:
+        return "gyro"
+    if data_type == MAGNETOMETER:
+        return "magnetometer"
+    if data_type == DEVICEMOTION:
+        return "devicemotion"
+    if data_type == REACHABILITY:
+        return "reachability"
+    if data_type == IOS_LOG_FILE:
+        return "ios_log"
+    if data_type == IMAGE_FILE:
+        "imageSurvey"
     raise Exception("unknown data type: %s" % data_type)
+
 
 CHUNKABLE_FILES = {ACCELEROMETER,
                    BLUETOOTH,
@@ -280,7 +299,7 @@ CHUNKABLE_FILES = {ACCELEROMETER,
                    REACHABILITY,
                    IOS_LOG_FILE}
 
-## Survey Question Types
+# Survey Question Types
 FREE_RESPONSE = "free_response"
 CHECKBOX = "checkbox"
 RADIO_BUTTON = "radio_button"
@@ -291,42 +310,43 @@ ALL_QUESTION_TYPES = {FREE_RESPONSE,
                       CHECKBOX,
                       RADIO_BUTTON,
                       SLIDER,
-                      INFO_TEXT_BOX }
+                      INFO_TEXT_BOX}
 
 NUMERIC_QUESTIONS = {RADIO_BUTTON,
                      SLIDER,
-                     FREE_RESPONSE }
+                     FREE_RESPONSE}
 
-## Free Response text field types (answer types)
+# Free Response text field types (answer types)
 FREE_RESPONSE_NUMERIC = "NUMERIC"
 FREE_RESPONSE_SINGLE_LINE_TEXT = "SINGLE_LINE_TEXT"
 FREE_RESPONSE_MULTI_LINE_TEXT = "MULTI_LINE_TEXT"
 
 TEXT_FIELD_TYPES = {FREE_RESPONSE_NUMERIC,
                     FREE_RESPONSE_SINGLE_LINE_TEXT,
-                    FREE_RESPONSE_MULTI_LINE_TEXT }
+                    FREE_RESPONSE_MULTI_LINE_TEXT}
 
-## Comparators
+# Comparators
 COMPARATORS = {"<",
                ">",
                "<=",
                ">=",
                "==",
-               "!=" }
+               "!="}
 
 NUMERIC_COMPARATORS = {"<",
                        ">",
                        "<=",
-                       ">=" }
+                       ">="}
 
-## Password Check Regexes
+# Password Check Regexes
 SYMBOL_REGEX = "[^a-zA-Z0-9]"
 LOWERCASE_REGEX = "[a-z]"
 UPPERCASE_REGEX = "[A-Z]"
 NUMBER_REGEX = "[0-9]"
 PASSWORD_REQUIREMENT_REGEX_LIST = [SYMBOL_REGEX, LOWERCASE_REGEX, UPPERCASE_REGEX, NUMBER_REGEX]
 
-DEVICE_IDENTIFIERS_HEADER = "patient_id,MAC,phone_number,device_id,device_os,os_version,product,brand,hardware_id,manufacturer,model,beiwe_version\n"
+DEVICE_IDENTIFIERS_HEADER = "patient_id,MAC,phone_number,device_id,device_os,os_version,product,brand,hardware_id," \
+                            "manufacturer,model,beiwe_version\n"
 
 # Encryption constants
 ASYMMETRIC_KEY_LENGTH = 2048  # length of private/public keys
@@ -343,5 +363,7 @@ class ResearcherRole(object):
     study_admin = "study_admin"
     researcher = "study_researcher"
 
+
 ALL_RESEARCHER_TYPES = (ResearcherRole.study_admin, ResearcherRole.researcher)
 ALL_JOB_TYPES = ["copy_to_box", "run_pipeline"]
+PIPELINE_THREADS = 8
