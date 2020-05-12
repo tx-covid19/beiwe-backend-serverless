34fbbf8317d42f649bdb0bdb7435a6038be807a7
Pu Zhao <puzhao@utexas.edu>
2020-05-12 11:12:42 -0500

    update gps upload

--- api/mindlogger/gps_api.py
+++ api/mindlogger/gps_api.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from typing import List
 
-from flask import Blueprint, jsonify, request, abort
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import (
     jwt_required, get_jwt_identity
 )
@@ -12,39 +11,38 @@ from database.user_models import Participant
 gps_api = Blueprint('gps_api', __name__)
 
 
+# {
+#     "duration": 3600008,
+#     "end_time": 1589227819,
+#     "distance": 123.34,
+#     "standard_deviation": 6.1,
+#     "n": 7
+# }
 @gps_api.route('/gps', methods=['POST'])
 @jwt_required
 def add_gps_measure():
     if not request.is_json:
-        return abort(400)
+        return jsonify({'msg': 'Must be a json body.'}), 400
 
     patient_id = get_jwt_identity()
     try:
         user = Participant.objects.get(patient_id__exact=patient_id)
     except:
-        return abort(400)
+        return jsonify({'msg': 'No access'}), 401
 
-    data: List[dict] = request.json
-    count = 0
+    data = request.json
+    try:
+        timestamp = int(data['end_time'])
+        distance = float(data['distance'])
+        standard_deviation = float(data['standard_deviation'])
+        count = int(data['n'])
+        duration = int(data['duration'])
+        if duration < 0 or count < 0 or distance < 0:
+            return jsonify({'msg': 'Bad data.'}), 400
 
-    if not isinstance(data, list):
-        return jsonify({'msg': 'Must be an array!'})
-
-    for dp in data:
-        if any([key not in dp for key in ['timestamp', 'distance', 'standard_deviation']]):
-            continue
-        try:
-            timestamp = int(dp['timestamp'])
-            distance = float(dp['distance'])
-            standard_deviation = float(dp['standard_deviation'])
-            updated_time = datetime.utcfromtimestamp(timestamp)
-            if distance < 0:
-                continue
-
-            GPSMeasure(user=user, last_updated=updated_time, distance=distance,
-                       standard_deviation=standard_deviation).save()
-            count += 1
-        except:
-            pass
-
-    return jsonify({'msg': '{}/{} has been saved.'.format(count, len(data))})
+        updated_time = datetime.utcfromtimestamp(timestamp)
+        GPSMeasure(user=user, last_updated=updated_time, n=count, duration=duration, distance=distance,
+                   standard_deviation=standard_deviation).save()
+        return jsonify({'msg': 'GPS updated.'}), 200
+    except:
+        return jsonify({'msg': 'Bad data.'}), 400
--- database/gps_models.py
+++ database/gps_models.py
@@ -6,5 +6,7 @@ from database.user_models import Participant
 class GPSMeasure(models.Model):
     user = models.ForeignKey(Participant, on_delete=models.PROTECT)
     last_updated = models.DateTimeField()
+    duration = models.PositiveIntegerField()
     distance = models.FloatField()
     standard_deviation = models.FloatField()
+    n = models.PositiveIntegerField()
