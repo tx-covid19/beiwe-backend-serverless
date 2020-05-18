f5e66d9333276986c534ca5bcedd4185ed9c4fcc
Pu Zhao <puzhao@utexas.edu>
2020-05-17 23:29:17 -0500

    misc change for app.py

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from raven.contrib.flask import Sentry
 from werkzeug.middleware.proxy_fix import ProxyFix
+from config import load_django
 
 from api import (admin_api, copy_study_api, dashboard_api, data_access_api, data_pipeline_api, external_api,
-                 mobile_api, participant_administration, survey_api)
+                 mobile_api, participant_administration, survey_api, user_auth_api, info_api)
 from api.fitbit import fitbit_api
-from api.mindlogger import mlogger_user_api, applet_api, response_api, schedule_api, file_api, info_api, gps_api
 from api.redcap import redcap_api
-from config import load_django
+from api.mindlogger import manage_api, applet_api, response_api, schedule_api, file_api, gps_api
+from api.tracker import event_api, overview_api, refresh_api, tracker_api
 from config.settings import SENTRY_ELASTIC_BEANSTALK_DSN, SENTRY_JAVASCRIPT_DSN, DOMAIN_NAME, \
     BEIWE_SUBDOMAIN, DIGITAL_SELFIE_SUBDOMAIN, FITBIT_SUBDOMAIN, BEIWE_ROOT_DOMAIN, CDN_DOMAIN
+from database.token_models import TokenBlacklist
 from libs.admin_authentication import is_logged_in
 from libs.security import set_secret_key
 from pages import (admin_pages, data_access_web_form, mobile_pages, survey_designer,
@@ -32,11 +34,18 @@ def subdomain(directory):
     app.config['JWT_SECRET_KEY'] = app.secret_key
     app.config['JWT_HEADER_NAME'] = 'Girder-Token'
     app.config['JWT_HEADER_TYPE'] = ''
+    app.config['JWT_BLACKLIST_ENABLED'] = True
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
     app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
     app.config['CDN_DOMAIN'] = CDN_DOMAIN
 
     jwt = JWTManager(app)
+
+    @jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decoded_token):
+        return TokenBlacklist.is_blacklisted(decoded_token)
+
     loader = [app.jinja_loader, jinja2.FileSystemLoader(directory + "/templates")]
     app.jinja_loader = jinja2.ChoiceLoader(loader)
     app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -50,7 +59,6 @@ def subdomain(directory):
 print(
     f'Configuring {DOMAIN_NAME} with {BEIWE_SUBDOMAIN}, {DIGITAL_SELFIE_SUBDOMAIN}, {FITBIT_SUBDOMAIN}, static loaded from {CDN_DOMAIN}')
 
-load_django.load()
 app = subdomain("frontend")
 app.config['SERVER_NAME'] = BEIWE_ROOT_DOMAIN
 app.config['CDN_DOMAIN'] = CDN_DOMAIN
@@ -72,18 +80,29 @@ app.register_blueprint(copy_study_api.copy_study_api, subdomain=BEIWE_SUBDOMAIN)
 app.register_blueprint(data_pipeline_api.data_pipeline_api, subdomain=BEIWE_SUBDOMAIN)
 app.register_blueprint(dashboard_api.dashboard_api, subdomain=BEIWE_SUBDOMAIN)
 
+# Auth APIs, shared by tracker and mindlogger
+app.register_blueprint(user_auth_api.auth_api, url_prefix='/api/v1/user')
+
 # mindlogger (hornsense) APIs
-app.register_blueprint(mlogger_user_api.user_api, url_prefix='/api/v1/user')
+app.register_blueprint(manage_api.manage_api, url_prefix='/api/v1/user')
 app.register_blueprint(applet_api.applet_api, url_prefix='/api/v1/applet')
 app.register_blueprint(response_api.response_api, url_prefix='/api/v1/response')
 app.register_blueprint(schedule_api.schedule_api, url_prefix='/api/v1/schedule')
 app.register_blueprint(file_api.file_api, url_prefix='/api/v1/file')
-app.register_blueprint(info_api.info_api, url_prefix='/api/v1/info')
 app.register_blueprint(gps_api.gps_api, url_prefix='/api/v1')
+
+# covid19 information
+app.register_blueprint(info_api.info_api, url_prefix='/info')
 
 # selfie APIs
 app.register_blueprint(external_api.external_api, subdomain=BEIWE_SUBDOMAIN)
 app.register_blueprint(digital_selfie_web_form.digital_selfie_web_form, subdomain=DIGITAL_SELFIE_SUBDOMAIN)
+
+# tracker APIs
+app.register_blueprint(event_api.event_api, url_prefix='/tracker')
+app.register_blueprint(overview_api.overview_api, url_prefix='/tracker')
+app.register_blueprint(refresh_api.refresh_api, url_prefix='/tracker')
+app.register_blueprint(tracker_api.tracker_api, url_prefix='/tracker')
 
 # redcap integration
 app.register_blueprint(redcap_api.redcap_api, subdomain=BEIWE_SUBDOMAIN)
@@ -94,12 +113,6 @@ app.register_blueprint(fitbit_api.fitbit_api, subdomain=FITBIT_SUBDOMAIN)
 # Don't set up Sentry for local development
 if os.environ['DJANGO_DB_ENV'] != 'local':
     sentry = Sentry(app, dsn=SENTRY_ELASTIC_BEANSTALK_DSN)
-
-
-# @jwt.token_in_blacklist_loader
-# def check_if_token_in_blacklist(decrypted_token):
-#     jti = decrypted_token['jti']
-#     return jti in blacklist
 
 
 @app.route("/<page>.html")
