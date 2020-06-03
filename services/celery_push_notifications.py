bc813a62a038a838cd8e5188d360afcce65fa800
Eli Jones <eli@zagaran.com>
2020-06-03 03:46:01 -0400

    [development] potentially buggy send push notification logic, now batches notifications.

--- services/celery_push_notifications.py
+++ services/celery_push_notifications.py
@@ -3,19 +3,21 @@ from sys import path
 # add the root of the project into the path to allow cd-ing into this folder and running the script.
 path.insert(0, abspath(__file__).rsplit('/', 2)[0])
 
+import json
+from collections import defaultdict
 from datetime import datetime, timedelta
+from typing import List
 
 import pytz
 from django.utils.timezone import make_aware
-from firebase_admin.messaging import Message, send
+from firebase_admin.messaging import Message, QuotaExceededError, send, UnregisteredError
 from kombu.exceptions import OperationalError
 
 from config.constants import API_TIME_FORMAT, PUSH_NOTIFICATION_SEND_QUEUE, ScheduleTypes
 from database.schedule_models import ScheduledEvent
-from database.survey_models import Survey
 from database.user_models import Participant
 from libs.celery_control import push_send_celery_app
-from libs.push_notifications import firebase_app, FirebaseNotCredentialed, set_next_weekly
+from libs.push_notifications import (firebase_app, FirebaseNotCredentialed, set_next_weekly)
 from libs.sentry import make_error_sentry
 
 
@@ -23,25 +25,44 @@ from libs.sentry import make_error_sentry
 ############################# Data Processing ##################################
 ################################################################################
 
+def get_surveys_and_schedules(now):
+    """ Mostly this function exists to reduce namespace clutter. """
+    # (don't trust django caching, just get it as a list...)
+    query = list(
+        ScheduledEvent.objects.filter(
+            scheduled_time__lte=now, participant__fcm_instance_id__isnull=False
+        ).exclude(
+            participant__fcm_instance_id=""
+        ).values_list(
+            "survey__object_id", "participant__fcm_instance_id", "pk"
+        )
+    )
+    # defaultdicts are clean; screw performance.
+    surveys = defaultdict(list)
+    schedules = defaultdict(list)
+    # we want a mapping of fcm tokens (a proxy for participants) to surveys and schedule ids (pks)
+    for survey_obj_id, fcm, schedule_id in query:
+        surveys[fcm].append(survey_obj_id)
+        schedules[fcm].append(schedule_id)
+
+    return dict(surveys), dict(schedules)
+
 
 def create_push_notification_tasks():
+    # we reuse the high level strategy from data processing celery tasks, see that documentation.
     expiry = (datetime.now() + timedelta(minutes=5)).replace(second=30, microsecond=0)
     now = make_aware(datetime.utcnow(), timezone=pytz.utc)
-    event_query = ScheduledEvent.objects.filter(
-        scheduled_time__lte=now, participant__fcm_instance_id__isnull=False
-    ).exclude(
-        participant__fcm_instance_id=""
-    ).values_list(
-        "survey__object_id", "participant__fcm_instance_id", "pk"
-    )
+    surveys, schedules = get_surveys_and_schedules(now)
 
     with make_error_sentry('data'):
         if not firebase_app:
             raise FirebaseNotCredentialed("Firebase is not configured, cannot queue notifications.")
 
-        for survey_obj_id, fcm_token, sched_pk in event_query:
+        # surveys and schedules are guaranteed to have the same keys, assembling the data structures
+        # is a pain, so it is factored out. sorry, but not sorry. it was a mess.
+        for fcm_token in surveys.keys():
             safe_queue_push(
-                args=[survey_obj_id, fcm_token, sched_pk],
+                args=[fcm_token, surveys[fcm_token], schedules[fcm_token]],
                 max_retries=0,
                 expires=expiry,
                 task_track_started=True,
@@ -51,44 +72,58 @@ def create_push_notification_tasks():
 
 
 @push_send_celery_app.task(queue=PUSH_NOTIFICATION_SEND_QUEUE)
-def celery_send_push_notification(survey_obj_id: str, fcm_token: str, sched_pk: int):
+def celery_send_push_notification(fcm_token: str, survey_obj_ids: List[str], schedule_pks: List[int]):
     ''' Celery task that sends push notifications. '''
-    with make_error_sentry("data"):
 
+    success = False
+    with make_error_sentry("data"):
         if not firebase_app:
             raise FirebaseNotCredentialed(
-                "You have not provided credentials for Firebase, notifications cannot be sent"
+                "You have not provided credentials for Firebase, notifications cannot be sent."
             )
 
-        # This if the schedule object doesn't exist, error.  This indicates a broken
-        schedule = ScheduledEvent.objects.get(pk=sched_pk)
-        is_weekly = schedule.get_schedule_type() == ScheduleTypes.weekly
+        # use the earliest timed schedule as our reference for the sent_time parameter.  (why?)
+        schedules = ScheduledEvent.objects.filter(pk__in=schedule_pks)
+        reference_schedule = schedules.order_by("scheduled_time").first()
 
-        # do not remove this assignment (yet), its presence in the namespace may have meaning.
-        response = send(Message(
-            data={
-                'type': 'survey',
-                'survey_id': survey_obj_id,
-                'sent_time': schedule.scheduled_time.strftime(API_TIME_FORMAT),
-            },
-            token=fcm_token,
-        ))
+        try:
+            # There is debugging code that looks for this variable, don't delete it...
+            response = send(Message(
+                data={
+                    'type': 'survey',
+                    'survey_ids': json.dumps(survey_obj_ids),
+                    'sent_time': reference_schedule.scheduled_time.strftime(API_TIME_FORMAT),
+                },
+                token=fcm_token,
+            ))
+            success = True
+        except UnregisteredError:
+            # TODO: mark participant token as out of date?  We don't know how reliable this is.
+            # get_next_weekly_event(Survey.objects.get(object_id=survey_obj_id))
+            pass
+        except QuotaExceededError:
+            # limits are very high, this is effectively impossible, but it is possible.
+            raise
 
+        # DEBUG uncomment to print
+        # from pprint import pprint
         # if err_sentry.errors:
         #     err_sentry.raise_errors()
         #
         # if "response" in vars():
         #     print(response)
-        #     from pprint import pprint
         #     pprint(vars(response))
 
-        schedule.archive()
-
-        if is_weekly:
-            set_next_weekly(
-                Participant.objects.get(fcm_instance_id=fcm_token),
-                Survey.objects.get(object_id=survey_obj_id),
-            )
+    # NOTE: code has exited the ErrorHandler.
+    # If the query was successful archive the schedules.
+    if success:
+        for schedule in schedules:
+            schedule.archive()
+            if schedule.get_schedule_type() == ScheduleTypes.weekly:
+                set_next_weekly(
+                    Participant.objects.get(fcm_instance_id=fcm_token),
+                    schedule.survey
+                )
 
 
 celery_send_push_notification.max_retries = 0
