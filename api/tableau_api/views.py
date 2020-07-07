815ac65c23eb3af2db5f479c51bd04fcd7769364
Caleb <calebthomasdixon@gmail.com>
2020-07-06 21:32:19 -0400

    added base functionality for tableau api queries

--- api/tableau_api/views.py
+++ api/tableau_api/views.py
@@ -1,5 +1,7 @@
 from api.tableau_api.base import TableauApiView
-
+from database.tableau_api_models import SchemaTableauAPI
+from django.core import serializers
+from django.shortcuts import  HttpResponse
 
 class SummaryStatisticDailyStudyView(TableauApiView):
     """
@@ -7,10 +9,17 @@ class SummaryStatisticDailyStudyView(TableauApiView):
     """
     path = '/api/v0/studies/<string:study_id>/summary-statistics/daily'
 
-    def get(self, study_id):
-        # Todo (CD): Implement SummaryStatisticDaily database model
-        # Todo (CD): Implement this logic
-        return 'ok'
+    def get(self, study_id, end_date=None, start_date=None, fields=None, limit=None, ordered_by='Date',
+            order_direction='descending', participant_ids=None):
+
+        queryset = SchemaTableauAPI.objects.filter(study__id=study_id)  # TODO (CD) lots more filters
+
+        # lots of ways to do this, but this is drawn from the docs
+        JSONSerializer = serializers.get_serializer("json")
+        json_serializer = JSONSerializer()
+        json_serializer.serialize(queryset)
+        data = json_serializer.getvalue()  # possibly useful to optimize to write to a file directly
+        return HttpResponse(data)
 
 
 # Todo (CD): Implement SummaryStatisticDailyParticipantView
