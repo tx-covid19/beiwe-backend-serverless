e15f27132394c83307b27a409b93ae1f0f5a37d1
Caleb <calebthomasdixon@gmail.com>
2020-07-10 12:46:04 -0400

    added data validation and basic error handling

--- api/tableau_api/views.py
+++ api/tableau_api/views.py
@@ -1,15 +1,27 @@
 from api.tableau_api.base import TableauApiView
 from database.tableau_api_models import SummaryStatisticDaily
 from django.core import serializers
-from django.shortcuts import HttpResponse
 from flask import request
 from datetime import date, datetime
 from dateutil.parser import parse
 from django.core.exceptions import FieldError
+from django import forms
+from database.study_models import Study
+from database.user_models import Participant
+from django.forms.models import model_to_dict
+from django.core.serializers import json
+
+
+class CleanSerializer(json.Serializer):
+    #  https://stackoverflow.com/questions/5453237/override-django-object-serializer-to-get-rid-of-specified-model
+    def get_dump_object(self, obj):
+        return self._current
 
 
 class DatabaseQueryFailed(Exception):
     status_code = 400
+
+
 class InvalidInput(Exception):
     status_code = 400
 
@@ -21,47 +33,156 @@ class SummaryStatisticDailyStudyView(TableauApiView):
     path = '/api/v0/studies/<string:study_id>/summary-statistics/daily'
 
     def get(self, study_id):
-        params = request.values
-        try:
-            queryset = self._query_database(study_id=study_id, **params)
-        except (TypeError, FieldError) as err:  # unexpected keyword
-            return self._render_error(err)
-
-        JSONSerializer = serializers.get_serializer("json")
-        json_serializer = JSONSerializer()
-        json_serializer.serialize(queryset, fields=params.get('fields', None))
-
+        request.values = dict(request.values)
+        request.values['study_id'] = study_id
+        errors, query = validate_query(**request.values)
+        if errors:
+            return errors
+        queryset = self._query_database(**query)
+        json_serializer = CleanSerializer()
+        json_serializer.serialize(queryset, fields=query['fields'])
         data = json_serializer.getvalue()  # possibly useful to optimize to write to a file directly/stream?
         return data
 
     def _query_database(self, study_id, end_date=None, start_date=None, limit=None, ordered_by='date',
-                        order_direction='descending', participant_ids=None, **_):
+                        order_direction='descending', participant_ids=None, fields=None):
         """
-        note: the 'fields' parameter is also used by API, but handled directly in the get function
+        study_id : int
+        end_date/start_date : date object
+        limit: int
+        ordered_by : string drawn from the list of fields
+        order_direction: string, either ascending/descending
+        participant_ids : list of ints
+        fields: any
         """
         if order_direction.lower() == 'descending':
             ordered_by = '-' + ordered_by
-        elif order_direction.lower() == 'ascending':
-            pass
-        else:
-            raise InvalidInput('If provided, the "ordered-direction" parameter is expected to have a value of '
-                               '"ascending" or "descending"')
-
-        queryset = SummaryStatisticDaily.objects.filter(study__id=study_id)
+        queryset = SummaryStatisticDaily.objects.filter(study__object_id=study_id)
         if participant_ids:
-            participant_ids = participant_ids.split(',')
             queryset = queryset.filter(participant__patient_id__in=participant_ids)
         if end_date:
-            queryset = queryset.filter(date__lte=parse(end_date))
+            queryset = queryset.filter(date__lte=end_date)
         if start_date:
-            queryset = queryset.filter(date__gte=parse(start_date))
+            queryset = queryset.filter(date__gte=start_date)
         queryset = queryset.order_by(ordered_by)
         if limit:
             queryset = queryset[:int(limit)]  # consider edge cases + queryset limit
         return queryset
 
-    def _render_error(self, err):
-        return 1  # TODO
 
+
+field_names = ["participant",
+               "study",
+               "date",
+               "distance_diameter",
+               "distance_from_home",
+               "distance_travelled",
+               "flight_distance_average",
+               "flight_distance_standard_deviation",
+               "flight_duration_average",
+               "flight_duration_standard_deviation",
+               "gps_data_missing_duration",
+               "home_duration",
+               "physical_circadian_rhythm",
+               "physical_circadian_rhythm_stratified",
+               "radius_of_gyration",
+               "significant_location_count",
+               "significant_location_entropy",
+               "stationary_fraction",
+               "text_incoming_count",
+               "text_incoming_degree",
+               "text_incoming_length",
+               "text_incoming_responsiveness",
+               "text_outgoing_count",
+               "text_outgoing_degree",
+               "text_outgoing_length",
+               "text_reciprocity",
+               "call_incoming_count",
+               "call_incoming_degree",
+               "call_incoming_duration",
+               "call_incoming_responsiveness",
+               "call_outgoing_count",
+               "call_outgoing_degree",
+               "call_outgoing_duration",
+               "acceleration_direction",
+               "accelerometer_coverage_fraction",
+               "accelerometer_signal_variability",
+               "accelerometer_univariate_summaries",
+               "device_proximity",
+               "total_power_events",
+               "total_screen_events",
+               "total_unlock_events",
+               "awake_onset_time",
+               "sleep_duration",
+               "sleep_onset_time"]
+# TODO, error messages, sensible defaults?
+class ApiQueryForm(forms.Form):
+    study_id = forms.ModelChoiceField(queryset=Study.objects.all(),
+                                      required=True)
+    # overwritten to clean to a string
+
+    end_date = forms.DateField(required=False)
+
+    start_date = forms.DateField(required=False)
+
+    limit = forms.IntegerField(required=False)
+
+    ordered_by = forms.ChoiceField(choices=[(f, f) for f in field_names],
+                                   required=False)
+
+    order_direction = forms.ChoiceField(choices=[('ascending', 'ascending'), ('descending', 'descending')],
+                                        required=False)
+
+    participant_ids = forms.ModelMultipleChoiceField(queryset=Participant.objects.all(),
+                                                     required=False)
+
+    fields = forms.MultipleChoiceField(choices=[(f, f) for f in field_names],
+                                       required=False)
+
+    def clean_study_id(self):
+        data = self.cleaned_data['study_id']
+        return data.object_id
+
+    def clean_participant_ids(self):
+        data = self.cleaned_data['participant_ids']
+        data = [str(d.patient_id) for d in data]
+        if not data:
+            return None
+        return data
+        # queryset -> list of strings or None
+
+    def clean_fields(self):
+        data = self.cleaned_data['fields']
+        if not data:
+            return None
+        return data
+
+
+def validate_query(**kwargs):
+    fields = kwargs.get('fields', '')
+    fields = fields.split(',')
+    if fields == ['']:
+        fields = []
+
+    participant_ids = kwargs.get('participant_ids', '')
+    participant_ids = participant_ids.split(',')
+    if participant_ids == ['']:
+        participant_ids = Participant.objects.none()
+
+    query = {'study_id': kwargs.get('study_id'),
+             'end_date': kwargs.get('end_date', None),
+             'start_date': kwargs.get('start_date', None),
+             'limit': kwargs.get('limit', None),
+             'ordered_by': kwargs.get('ordered_by', 'date'),
+             'order_direction': kwargs.get('order_direction', 'descending'),
+             'participant_ids': participant_ids,
+             'fields': fields}
+
+
+
+    form = ApiQueryForm(query)
+    if not form.is_valid():
+        return form.errors.as_json(), None
+    return None, form.cleaned_data
 
 # Todo (CD): Implement SummaryStatisticDailyParticipantView
