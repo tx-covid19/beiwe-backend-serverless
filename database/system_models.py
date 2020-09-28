ed749ca0299806dffb7dd44214d7d1ded21cc30f
Eli Jones <eli@zagaran.com>
2020-09-28 18:07:56 -0400

    [firebase-config] adds a new database table for storing credentials.

--- database/system_models.py
+++ database/system_models.py
@@ -32,3 +32,17 @@ class FileProcessLock(TimestampedModel):
     def get_time_since_locked(cls):
         return timezone.now() - FileProcessLock.objects.last().lock_time
 
+
+
+class FileAsText(TimestampedModel):
+    tag = models.CharField(required=True, max_length=256, db_index=True)
+    text = models.TextField(required=True)
+
+    # todo: store the files for sending push notifications as three entries in this database table
+    #  and make a function that checks for ios, android, and any push notifications to be present
+    #  and returns a boolean.
+    # todo: make a frontend page (system admins only) that allows you to upload (paste? whatever)
+    #  makes more sense the three push notification credential files, and then VALIDATES them,
+    #  tells the user what failed, and saves them.  (3 separate post operations, 1 each, not 3 in 1)
+
+
