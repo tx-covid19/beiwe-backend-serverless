2412a566449c61c6ab2f4e00967306ddaa49f9ac
Eli Jones <eli@zagaran.com>
2021-02-04 00:15:27 -0500

    [fix-dsns] should allow use of new-style DSNs.

--- libs/sentry.py
+++ libs/sentry.py
@@ -8,25 +8,37 @@ from config.settings import (SENTRY_DATA_PROCESSING_DSN, SENTRY_ELASTIC_BEANSTAL
 from libs.logging import log_error
 
 
-def get_dsn_from_string(sentry_type):
+def normalize_sentry_dsn(dsn: str):
+    if not dsn:
+        return dsn
+    # "https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "sub.domains.sentry.io/yyyyyy"
+    prefix, sentry_io = dsn.split("@")
+    if sentry_io.count(".") > 1:
+        # sub.domains.sentry.io/yyyyyy -> sentry.io/yyyyyy
+        sentry_io = ".".join(sentry_io.rsplit(".", 2)[-2:])
+    # https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx + @ + sentry.io/yyyyyy"
+    return prefix + "@" + sentry_io
+
+
+def get_dsn_from_string(sentry_type: str):
     """ Returns a DSN, even if it is incorrectly formatted. """
     if sentry_type == 'data':
-        return SENTRY_DATA_PROCESSING_DSN
+        return normalize_sentry_dsn(SENTRY_DATA_PROCESSING_DSN)
     elif sentry_type == 'eb':
-        return SENTRY_ELASTIC_BEANSTALK_DSN
+        return normalize_sentry_dsn(SENTRY_ELASTIC_BEANSTALK_DSN)
     elif sentry_type == 'js':
-        return SENTRY_JAVASCRIPT_DSN
+        return normalize_sentry_dsn(SENTRY_JAVASCRIPT_DSN)
     else:
-        raise RuntimeError('Invalid sentry type')
+        raise Exception('Invalid sentry type')
 
 
-def make_sentry_client(sentry_type, tags=None):
+def make_sentry_client(sentry_type: str, tags=None):
     dsn = get_dsn_from_string(sentry_type)
     tags = tags or {}
     return SentryClient(dsn=dsn, tags=tags, transport=HTTPTransport)
     
 
-def make_error_sentry(sentry_type, tags=None, null=False):
+def make_error_sentry(sentry_type:str, tags:dict=None, null:bool=False):
     """ Creates an ErrorSentry, defaults to error limit 10.
     If the applicable sentry DSN is missing will return an ErrorSentry,
     but if null truthy a NullErrorHandler will be returned instead. """
